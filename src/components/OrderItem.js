23d1883e3d9c5072460076e9fe4df41271dbe0b8
lucianobizin <lucianobizin@gmail.com>
2024-02-23 17:33:40 +0100

    Subiendo lo último de clase10

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -6,10 +6,8 @@ const OrderItem = ({order}) => {
   return (
     <View style={styles.card}>
             <View style={styles.textContainer}>      
-             <Text style={styles.text}>
-                {new Date (order.createdAt).toLocaleString()}
-             </Text>
-             <Text style={styles.text2}>$ {order.total}</Text>
+             <Text style={styles.text}>Creado: {new Date (order.createdAt).toLocaleString()}</Text>
+             <Text style={styles.text2}>Total: $ {order.total}</Text>
       </View>
       <Feather name="search" size={30} color="black"/>
     </View>
